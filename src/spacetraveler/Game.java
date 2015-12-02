12b60db52a6125fe43e05a98a45fd725ebfbb212
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2015-12-02 15:33:13 +0100

    Einfaches Fenster

--- src/spacetraveler/Game.java
+++ src/spacetraveler/Game.java
@@ -1,9 +1,25 @@
 package spacetraveler;
 
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event.Type;
+
 public class Game {
 
 	public static void main(String args[]){
+		RenderWindow hauptfenster = new RenderWindow(new VideoMode(1024, 800), "SpaceTraveler");
+		hauptfenster.clear();
 		
+		while(hauptfenster.isOpen()){
+			for(org.jsfml.window.event.Event ev : hauptfenster.pollEvents()){
+        		if(ev.type == Type.CLOSED){
+        			hauptfenster.close();
+        		}
+			}
+			
+
+			hauptfenster.display();
+		}
 	}
 	
 }
